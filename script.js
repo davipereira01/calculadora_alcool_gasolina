b466e24417a2bff8a5e2870d307a06c85ba059c7
davipereira01 <davi_e.profissional21@hotmail.com>
2021-11-07 11:40:26 -0300

    Alterações e ajustes no código JavaScript
    Alteração no estilo de botão e ajuste no código JavaScript, para que todas as premissas do trabalho fossem cumpridas.

--- script.js
+++ script.js
@@ -1,37 +1,65 @@
-var alcPrice = document.querySelector('#alc');
-var gasPrice = document.querySelector('#gas');
+
+function calcVantagem() {
+
+    let alcPrice = document.querySelector('#alc');
+    let gasPrice = document.querySelector('#gas');
+    let info = document.querySelector('.info');
+    let recommendation = document.querySelector('.recommendation');
+    let modalTitle = document.querySelector('.modal-title');
 
 
-function calcVantagem () {
+    let result = (parseFloat(alcPrice.value) / parseFloat(gasPrice.value));
 
-    var result = (parseFloat(alcPrice.value) / parseFloat(gasPrice.value));
+    if (isNaN(result) ) {
 
-    if (result > 0.7 ) {
+        alert('Você esqueceu de preencher um dos valores. Verifique e tente novamente');
+        
+    }
+
+    else {
+        if (result > 0.7) {
+
+        modalTitle.innerHTML = "É mais vantajoso abastecer com gasolina"
+        recommendation.innerHTML = " Abasteça com gasolina para economizar!"
+        }
+        else{
+            if (result == 0.7) {
+                modalTitle.innerHTML = "Escolha o combustivel de sua preferencia"
+                recommendation.innerHTML = "Nesse caso não há diferença entre álcool ou gasolina!"
+            }
+            else {
+
+                modalTitle.innerHTML = "É mais vantajoso abastecer com Álcool"
+                recommendation.innerHTML = " Abasteça com álcool para economizar!"
+            }
+        }
+        info.innerHTML = (("Verificamos que a relação de consumo é de ") + parseFloat(result.toFixed(2)));
+
         $('#modalGas').modal('show');
-    }else {
-        $('#modalAlc').modal('show');
+    } 
 
 }
+// A variavel abaixo chama a função que calcula a vantagem//
+const btnCalc = document.querySelector('#btnCalc').onclick = calcVantagem;
+
+// A funçao abaixo recarrega a página caso o usuario clique no botão "Calcular novamente"//
+
+function refreshPage() {
+
+    location.reload();
+
 }
 
+// A variavel abaixo recarrega a página caso o usuario clique no botão "Calcular novamente"// 
+
+const btnRefreshPage = document.querySelector('#refreshPage').onclick = refreshPage;
 
 
 
 
 
-var btnRefreshAlc = document.querySelector("#refreshAlc");
 
-btnRefreshAlc.addEventListener("click", function() {
-    
-    location.reload();
-});
 
-var btnRefreshGas = document.querySelector("#refreshGas");
-
-btnRefreshGas.addEventListener("click", function() {
-    
-    location.reload();
-});
 
 
 
